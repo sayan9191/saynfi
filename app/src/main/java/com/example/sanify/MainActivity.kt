2d2f868ab2c128d80f331d9eb680ff0084485d36
santanu <santanu.m@mantralabsglobal.com>
2023-04-27 21:06:49 +0530

    spinner added

--- app/src/main/java/com/example/sanify/MainActivity.kt
+++ app/src/main/java/com/example/sanify/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.sanify
 
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bluehomestudio.luckywheel.WheelItem
 import com.example.sanify.databinding.ActivityMainBinding
 import com.example.sanify.ui.lottery.LotteryActivity
 import com.example.sanify.ui.spin.LuckyDrawActivity
@@ -25,5 +28,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LuckyDrawActivity::class.java)
             startActivity(intent)
         }
+
     }
 }
